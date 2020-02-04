ee082ca845c605ef0e25ba25a807f5a9b647f9db
JooooHyeWon <JooooHyeWon@users.noreply.github.com>
2020-02-04 09:46:19 +0900

    0204 event 수정

--- meetme2/src/main/webapp/resources/js/event/newEvent.js
+++ meetme2/src/main/webapp/resources/js/event/newEvent.js
@@ -1,10 +1,12 @@
 
 jQuery(document).ready(function($) {
-	
+	var date = new Date();
+	date.setDate(date.getDate() + 1);
+
 	var picker = new Pikaday({ 
 		 field: document.getElementById('event_date'),
 		 format: 'yyyy-MM-dd',
-		 minDate : new Date(),
+		 minDate : date,
 		 toString(date, format) {
 		   let day = ("0" + date.getDate()).slice(-2);
 		   let month = ("0" + (date.getMonth() + 1)).slice(-2);
@@ -13,31 +15,12 @@ jQuery(document).ready(function($) {
 		 }
 		});
 	
-	var now = new Date();
-	var h = now.getHours();
-	var ap;
-	if(h>11){
-		ap = "PM";
-		h = h - 12;
-	}else{
-		ap = "AM";
-	}
-	var m = now.getMinutes();
-	if(m>0){
-		m = 30;
-	}else{
-		m = 0;
-	}
-	now = ap + " " + h + " : " + m;
-	console.log("현재 = "+now);
-	console.log("현재시 = "+h);
-	console.log("현재분 = "+m);
 
 	$("#event_time").timepicker({
 		step: 30,           			//시간간격 : 30분
 		timeFormat: "A h:i" ,   		// AM 시간:분 으로표시
 		closeOnWindowScroll : true, 	// 스크롤 내리면 선택기 닫기
-		minTime : now,			// 기본 시간
+		minTime : "AM 00:00",			// 기본 시간
 		maxTime : "PM 11:50",
 
 	});
